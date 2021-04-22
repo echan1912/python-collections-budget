97aa67b38d84c86820f06866b28f50829c41119a
echan1912 <echan1912@gmail.com>
2021-04-22 20:11:33 +0800

    edit

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,6 +1,6 @@
 from . import Expense
 
-class BudgetList:
+class BudgetList():
     def __init__(self, budget):
         self.budget = budget
         self.sum_expenses = 0
@@ -8,3 +8,25 @@ class BudgetList:
         self.sum_overages = 0
         self.overages = []
 
+def append(self, item):
+    if (self.sum_expenses+item < self.budget):
+        self.expenses.append(item)
+        self.sum_expenses += item
+    else:
+        self.overages.append(item)
+        self.sum_overages += item
+
+def __len__(self):
+    return len(self.expenses) + len(self.overages)
+
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+
+for expenses in expenses.list:
+    myBudgetList.append(expense.amount)
+print('The count of all expenses: ' + str(len(myBudgetList)))
+
+if __name__ == '__main__':
+    main()
