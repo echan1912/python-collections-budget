110a3582ec85b0e6dd4b33a4e608775a0edf054d
echan1912 <echan1912@gmail.com>
2021-04-22 22:36:10 +0800

    help

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -19,6 +19,8 @@ class BudgetList():
         return len(self.expenses) + len(self.overages)
     def __iter__(self):
         self.iter_e=iter(self.expenses)
+        self.iter_o=iter(self.overages)
+        return self
     def __next__(self):
         pass
 
