dffc9697fcd294b830d6ab93f2beeabb330d96f0
echan1912 <echan1912@gmail.com>
2021-04-23 13:45:37 +0800

    c

--- budget/Expense.py
+++ budget/Expense.py
@@ -45,6 +45,8 @@ class Expenses():
             if x.category == "Phone" or x.category == "Auto and Gas" or 
                x.category=="Classes" or x.category == "Utilities" or 
                x.category == "Mortgage"}
-        food_expenses ={x for x in self.list if x.category == 'Groceries' or x == 'Eating Out'}
+        food_expenses = {
+            x for x in self.list 
+                            if x.category == 'Groceries' or x.category == 'Eating Out'}
         unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
         return [necessary_expenses, food_expenses, unnecessary_expenses]
